ff3dba5e86ab7a28a7b9397fbf63694657eaebf6
JoaoPgith <165024755+JoaoPgith@users.noreply.github.com>
2024-09-09 09:52:26 -0300

    Create conta_poupanca.py

---
+++ conta_poupanca.py
@@ -0,0 +1,24 @@
+from conta_corrente import ContaCorrente
+
+class ContaPoupanca(ContaCorrente):
+    def __init__(self, numero, agencia, cliente, saldo, historico_transacoes,  taxa_juros):
+        super().__init__(numero, agencia, cliente, saldo, historico_transacoes)
+        self.taxa_juros = taxa_juros
+    
+    def saque(self, valor):
+        if valor > self.saldo:
+            print("Saldo insuficiente para saque.")
+        else:
+            self.saldo -= valor
+            self.historico_transacoes.append(f'Saque: R${valor:.2f}')
+            print(f'Saque de R${valor:.2f} realizado com sucesso')
+
+        
+    def deposito(self, valor):
+        self.saldo += valor
+        self.historico_transacoes.append(f'Saque: R${valor:.2f}')
+        print(f'Depósito de R${valor:.2f} realizado com sucesso')
+
+    def extrato(self):
+        print(f"Extrato da Conta {self.numero}:")
+        print(f"Saldo atual: R${self.saldo:.2f}")
