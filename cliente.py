5e4d6121f944c4d904c56c38dd939cbb408512c5
Joao Soares <jn910452@gmail.com>
2024-09-12 06:54:30 -0300

    cirrigindo erro class conta

--- cliente.py
+++ cliente.py
@@ -1,6 +1,6 @@
 from pessoa import Pessoa
 
-class cliente:
+class cliente(Pessoa):
     def __init__(self, nome, cpf, num_contato):
         super().__init__(nome, cpf)
         self.num_contato = num_contato
